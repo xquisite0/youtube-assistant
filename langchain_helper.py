4eb1e99a143454ebec4526bc0bc323130f1cb369
Zhou Yuhang <zyuhang255@gmail.com>
2025-08-07 10:23:42 +0800

    convert transcript to vector embeddings to store in vector database (FAISS)

--- langchain_helper.py
+++ langchain_helper.py
@@ -18,4 +18,5 @@ def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     docs = text_splitter.split_documents(transcript)
 
-    
+    db = FAISS.from_documents(docs, embeddings)
+    return db
