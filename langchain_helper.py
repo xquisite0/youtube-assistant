6b9ba1b1260b4d5753e9e87d8d062a45ed99bffa
Zhou Yuhang <zyuhang255@gmail.com>
2025-08-07 10:17:22 +0800

    initialise langchain imports

---
+++ langchain_helper.py
@@ -0,0 +1,12 @@
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
